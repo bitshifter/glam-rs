2990dd0b069ef4e2ba708035ef1d3e9d790415e3
Cameron Hart <cameron.hart@gmail.com>
2019-04-15 22:46:15 +1000

    Got rid of Vec4::shuffle and use intrinsics instead.

--- src/f32/mat4.rs
+++ src/f32/mat4.rs
@@ -183,23 +183,43 @@ impl Mat4 {
         }
     }
 
+    #[cfg(all(target_feature = "sse2", not(feature = "no-simd")))]
     #[inline]
     pub fn transpose(&self) -> Mat4 {
-        let tmp0 = self.x_axis.shuffle(self.y_axis, 0b00_01_00_01);
-        let tmp1 = self.x_axis.shuffle(self.y_axis, 0b10_11_10_11);
-        let tmp2 = self.z_axis.shuffle(self.w_axis, 0b00_01_00_01);
-        let tmp3 = self.z_axis.shuffle(self.w_axis, 0b10_11_10_11);
+        // sse2 implemenation based off DirectXMath XMMatrixInverse (MIT License)
+        #[cfg(target_arch = "x86")]
+        use std::arch::x86::*;
+        #[cfg(target_arch = "x86_64")]
+        use std::arch::x86_64::*;
 
-        let x_axis = tmp0.shuffle(tmp2, 0b00_10_00_10);
-        let y_axis = tmp0.shuffle(tmp2, 0b01_11_01_11);
-        let z_axis = tmp1.shuffle(tmp3, 0b00_10_00_10);
-        let w_axis = tmp1.shuffle(tmp3, 0b01_11_01_11);
+        unsafe {
+            let tmp0 = _mm_shuffle_ps(self.x_axis.0, self.y_axis.0, 0b01_00_01_00);
+            let tmp1 = _mm_shuffle_ps(self.x_axis.0, self.y_axis.0, 0b11_10_11_10);
+            let tmp2 = _mm_shuffle_ps(self.z_axis.0, self.w_axis.0, 0b01_00_01_00);
+            let tmp3 = _mm_shuffle_ps(self.z_axis.0, self.w_axis.0, 0b11_10_11_10);
+
+            Mat4 {
+                x_axis: _mm_shuffle_ps(tmp0, tmp2, 0b10_00_10_00).into(),
+                y_axis: _mm_shuffle_ps(tmp0, tmp2, 0b11_01_11_01).into(),
+                z_axis: _mm_shuffle_ps(tmp1, tmp3, 0b10_00_10_00).into(),
+                w_axis: _mm_shuffle_ps(tmp1, tmp3, 0b11_01_11_01).into(),
+            }
+        }
+    }
+
+    #[cfg(any(not(target_feature = "sse2"), feature = "no-simd"))]
+    #[inline]
+    pub fn transpose(&self) -> Mat4 {
+        let (m00, m01, m02, m03) = self.x_axis.into();
+        let (m10, m11, m12, m13) = self.y_axis.into();
+        let (m20, m21, m22, m23) = self.z_axis.into();
+        let (m30, m31, m32, m33) = self.w_axis.into();
 
         Mat4 {
-            x_axis,
-            y_axis,
-            z_axis,
-            w_axis,
+            x_axis: Vec4::new(m00, m10, m20, m30),
+            y_axis: Vec4::new(m01, m11, m21, m31),
+            z_axis: Vec4::new(m02, m12, m22, m32),
+            w_axis: Vec4::new(m03, m13, m23, m33),
         }
     }
 
@@ -478,303 +498,6 @@ impl Mat4 {
             }
         }
     }
-
-    // pub fn inverse_dxm(&self) -> Option<Mat4> {
-    //     let mt = self.transpose();
-    //     let mtx = mt.get_x_axis();
-    //     let mty = mt.get_y_axis();
-    //     let mtz = mt.get_z_axis();
-    //     let mtw = mt.get_w_axis();
-
-    //     // XMVECTOR V00 = XM_PERMUTE_PS(mtz,_MM_SHUFFLE(1,1,0,0));
-    //     let mut v00 = permute_xxyy(mtz);
-    //     // XMVECTOR V10 = XM_PERMUTE_PS(mtw,_MM_SHUFFLE(3,2,3,2));
-    //     let mut v10 = permute_zwzw(mtw);
-    //     // XMVECTOR V01 = XM_PERMUTE_PS(mtx,_MM_SHUFFLE(1,1,0,0));
-    //     let mut v01 = permute_xxyy(mtx);
-    //     // XMVECTOR V11 = XM_PERMUTE_PS(mty,_MM_SHUFFLE(3,2,3,2));
-    //     let mut v11 = permute_zwzw(mty);
-    //     // XMVECTOR V02 = _mm_shuffle_ps(mtz, mtx,_MM_SHUFFLE(2,0,2,0));
-    //     let mut v02 = shuffle_xzxz(mtz, mtx);
-    //     // XMVECTOR V12 = _mm_shuffle_ps(mtw, mty,_MM_SHUFFLE(3,1,3,1));
-    //     let mut v12 = shuffle_ywyw(mtw, mty);
-
-    //     let mut d0 = v00 * v10;
-    //     let mut d1 = v01 * v11;
-    //     let mut d2 = v02 * v12;
-
-    //     // V00 = XM_PERMUTE_PS(mtz,_MM_SHUFFLE(3,2,3,2));
-    //     v00 = permute_zwzw(mtz);
-    //     // V10 = XM_PERMUTE_PS(mtw,_MM_SHUFFLE(1,1,0,0));
-    //     v10 = permute_xxyy(mtw);
-    //     // V01 = XM_PERMUTE_PS(mtx,_MM_SHUFFLE(3,2,3,2));
-    //     v01 = permute_zwzw(mtx);
-    //     // V11 = XM_PERMUTE_PS(mty,_MM_SHUFFLE(1,1,0,0));
-    //     v11 = permute_xxyy(mty);
-    //     // V02 = _mm_shuffle_ps(mtz,mtx,_MM_SHUFFLE(3,1,3,1));
-    //     v02 = shuffle_ywyw(mtz, mtx);
-    //     // V12 = _mm_shuffle_ps(mtw,mty,_MM_SHUFFLE(2,0,2,0));
-    //     v12 = shuffle_xzxz(mtw, mty);
-
-    //     // v00 = _mm_mul_ps(v00 * v10);
-    //     // v01 = _mm_mul_ps(v01 * v11);
-    //     // v02 = _mm_mul_ps(v02 * v12);
-    //     // d0 = _mm_sub_ps(d0,v00);
-    //     // d1 = _mm_sub_ps(d1,v01);
-    //     // d2 = _mm_sub_ps(d2,v02);
-    //     d0 = v00.neg_mul_sub(v10, d0);
-    //     d1 = v01.neg_mul_sub(v11, d1);
-    //     d2 = v02.neg_mul_sub(v12, d2);
-
-    //     // V11 = D0Y,D0W,D2Y,D2Y
-    //     // V11 = _mm_shuffle_ps(D0,D2,_MM_SHUFFLE(1,1,3,1));
-    //     v11 = shuffle_ywyy(d0, d2);
-    //     // V00 = XM_PERMUTE_PS(mty, _MM_SHUFFLE(1,0,2,1));
-    //     v00 = permute_yzxy(mty);
-    //     // V10 = _mm_shuffle_ps(V11,D0,_MM_SHUFFLE(0,3,0,2));
-    //     v10 = shuffle_zxwx(v11, d0);
-    //     // V01 = XM_PERMUTE_PS(mtx, _MM_SHUFFLE(0,1,0,2));
-    //     v01 = permute_zxyx(mtx);
-    //     // V11 = _mm_shuffle_ps(V11,D0,_MM_SHUFFLE(2,1,2,1));
-    //     v11 = shuffle_yzyz(v11, d0);
-
-    //     // V13 = D1Y,D1W,D2W,D2W
-    //     // XMVECTOR V13 = _mm_shuffle_ps(D1,D2,_MM_SHUFFLE(3,3,3,1));
-    //     let v13 = shuffle_ywww(d1, d2);
-    //     // V02 = XM_PERMUTE_PS(mtw, _MM_SHUFFLE(1,0,2,1));
-    //     v02 = permute_yzxy(mtw);
-    //     // V12 = _mm_shuffle_ps(V13,D1,_MM_SHUFFLE(0,3,0,2));
-    //     v12 = shuffle_zxwx(v13, d1);
-    //     // XMVECTOR V03 = XM_PERMUTE_PS(mtz,_MM_SHUFFLE(0,1,0,2));
-    //     let v03 = permute_zxyx(mtz);
-    //     // V13 = _mm_shuffle_ps(V13,D1,_MM_SHUFFLE(2,1,2,1));
-    //     v13 = shuffle_yzyz(v13, d1);
-
-    //     let C0 = v00 * v10;
-    //     let C2 = v01 * v11;
-    //     let C4 = v02 * v12;
-    //     let C6 = v03 * v13;
-
-    //     // V11 = D0X,D0Y,D2X,D2X
-    //     // V11 = _mm_shuffle_ps(D0,D2,_MM_SHUFFLE(0,0,1,0));
-    //     v11 = shuffle_xyxx(d0, d2);
-    //     // V00 = XM_PERMUTE_PS(mty, _MM_SHUFFLE(2,1,3,2));
-    //     v00 = permute_zwyz(mty);
-    //     // V10 = _mm_shuffle_ps(D0,V11,_MM_SHUFFLE(2,1,0,3));
-    //     v10 = shuffle_wxyz(d0, v11);
-    //     // V01 = XM_PERMUTE_PS(mtx, _MM_SHUFFLE(1,3,2,3));
-    //     v01 = permute_wzwy(mtx);
-    //     // V11 = _mm_shuffle_ps(D0,V11,_MM_SHUFFLE(0,2,1,2));
-    //     v11 = shuffle_zyzx(d0, v11);
-
-    //     // V13 = D1X,D1Y,D2Z,D2Z
-    //     // V13 = _mm_shuffle_ps(D1,D2,_MM_SHUFFLE(2,2,1,0));
-    //     v13 = shuffle_xyzz(d1, d2);
-    //     // V02 = XM_PERMUTE_PS(mtw, _MM_SHUFFLE(2,1,3,2));
-    //     v02 = permute_zwyz(mtw);
-    //     // V12 = _mm_shuffle_ps(D1,V13,_MM_SHUFFLE(2,1,0,3));
-    //     v12 = shuffle_wxyz(d1, v13);
-    //     // V03 = XM_PERMUTE_PS(mtz,_MM_SHUFFLE(1,3,2,3));
-    //     v03 = permute_wzwx(mtz);
-    //     // V13 = _mm_shuffle_ps(D1,V13,_MM_SHUFFLE(0,2,1,2));
-    //     v13 = shuffle_zyzx(d1, v13);
-
-    //     // V00 = _mm_mul_ps(V00,V10);
-    //     // V01 = _mm_mul_ps(V01,V11);
-    //     // V02 = _mm_mul_ps(V02,V12);
-    //     // V03 = _mm_mul_ps(V03,V13);
-    //     // C0 = _mm_sub_ps(C0,V00);
-    //     // C2 = _mm_sub_ps(C2,V01);
-    //     // C4 = _mm_sub_ps(C4,V02);
-    //     // C6 = _mm_sub_ps(C6,V03);
-    //     c0 = v00.neg_mul_sub(v10, c0);
-    //     c2 = v01.neg_mul_sub(v11, c2);
-    //     c4 = v02.neg_mul_sub(v12, c4);
-    //     c6 = v03.neg_mul_sub(v13, c6);
-
-    //     // V00 = XM_PERMUTE_PS(mty,_MM_SHUFFLE(0,3,0,3));
-    //     v00 = permute_wxwx(mty);
-    //     // V10 = D0Z,D0Z,D2X,D2Y
-    //     // V10 = _mm_shuffle_ps(D0,D2,_MM_SHUFFLE(1,0,2,2));
-    //     v10 = shuffle_zzxy(d0, d2);
-    //     // V10 = XM_PERMUTE_PS(V10,_MM_SHUFFLE(0,2,3,0));
-    //     v10 = permute_xwzx(v10);
-    //     // V01 = XM_PERMUTE_PS(mtx,_MM_SHUFFLE(2,0,3,1));
-    //     v01 = permute_ywxz(mtx);
-    //     // V11 = D0X,D0W,D2X,D2Y
-    //     // V11 = _mm_shuffle_ps(D0,D2,_MM_SHUFFLE(1,0,3,0));
-    //     v11 = shuffle_xwxy(d0, d2);
-    //     // V11 = XM_PERMUTE_PS(V11,_MM_SHUFFLE(2,1,0,3));
-    //     v11 = permute_wxyz(v11);
-    //     // V02 = XM_PERMUTE_PS(mtw,_MM_SHUFFLE(0,3,0,3));
-    //     v02 = permute_wxwx(mtw);
-    //     // V12 = D1Z,D1Z,D2Z,D2W
-    //     // V12 = _mm_shuffle_ps(D1,D2,_MM_SHUFFLE(3,2,2,2));
-    //     v12 = shuffle_zzzw(d1, d2);
-    //     // V12 = XM_PERMUTE_PS(V12,_MM_SHUFFLE(0,2,3,0));
-    //     v12 = permute_xwzx(v12);
-    //     // V03 = XM_PERMUTE_PS(mtz,_MM_SHUFFLE(2,0,3,1));
-    //     v03 = permute_ywxz(mtz);
-    //     // V13 = D1X,D1W,D2Z,D2W
-    //     // V13 = _mm_shuffle_ps(D1,D2,_MM_SHUFFLE(3,2,3,0));
-    //     v13 = shuffle_xwzw(d1, d2);
-    //     // V13 = XM_PERMUTE_PS(V13,_MM_SHUFFLE(2,1,0,3));
-    //     v13 = permute_wxyz(v13);
-
-    //     // V00 = _mm_mul_ps(V00,V10);
-    //     // V01 = _mm_mul_ps(V01,V11);
-    //     // V02 = _mm_mul_ps(V02,V12);
-    //     // V03 = _mm_mul_ps(V03,V13);
-    //     // XMVECTOR C1 = _mm_sub_ps(C0,V00);
-    //     // C0 = _mm_add_ps(C0,V00);
-    //     // XMVECTOR C3 = _mm_add_ps(C2,V01);
-    //     // C2 = _mm_sub_ps(C2,V01);
-    //     // XMVECTOR C5 = _mm_sub_ps(C4,V02);
-    //     // C4 = _mm_add_ps(C4,V02);
-    //     // XMVECTOR C7 = _mm_add_ps(C6,V03);
-    //     // C6 = _mm_sub_ps(C6,V03);
-    //     let c1 = v00.neg_mul_sub(v10, c0);
-    //     C0 = v00.mul_add(v10, c0);
-    //     let c3 = v01.mul_add(v11, c2);
-    //     c2 = v01.neg_mul_sub(v11, c2);
-    //     let c5 = v02.neg_mul_sub(v12, c4);
-    //     c4 = v02.mul_add(v12, c4);
-    //     let c7 = v03.mul_add(v13, c6);
-    //     C6 = v03.neg_mul_sub(v13, c6);
-
-    //     // C0 = _mm_shuffle_ps(C0,C1,_MM_SHUFFLE(3,1,2,0));
-    //     c0 = shuffle_xzyw(c0, c1);
-    //     // C2 = _mm_shuffle_ps(C2,C3,_MM_SHUFFLE(3,1,2,0));
-    //     c2 = shuffle_xzyw(c2, c3);
-    //     // C4 = _mm_shuffle_ps(C4,C5,_MM_SHUFFLE(3,1,2,0));
-    //     c4 = shuffle_xzyw(c4, c5);
-    //     // C6 = _mm_shuffle_ps(C6,C7,_MM_SHUFFLE(3,1,2,0));
-    //     c6 = shuffle_xzyw(c6, c7);
-    //     // C0 = XM_PERMUTE_PS(C0,_MM_SHUFFLE(3,1,2,0));
-    //     c0 = permute_xzyw(c0);
-    //     // C2 = XM_PERMUTE_PS(C2,_MM_SHUFFLE(3,1,2,0));
-    //     c2 = permute_xzyw(c2);
-    //     // C4 = XM_PERMUTE_PS(C4,_MM_SHUFFLE(3,1,2,0));
-    //     c4 = permute_xzyw(c4);
-    //     // C6 = XM_PERMUTE_PS(C6,_MM_SHUFFLE(3,1,2,0));
-    //     c6 = permute_xzyw(c6);
-
-    //     // Get the determinate
-    //     // XMVECTOR vTemp = XMVector4Dot(C0,mtx);
-    //     let mut det = c0.dot(mtx);
-    //     // if (pDeterminant != nullptr)
-    //     //     *pDeterminant = vTemp;
-    //     if det == Vec4::zero() {
-    //         return None;
-    //     }
-    //     // vTemp = _mm_div_ps(g_XMOne,vTemp);
-    //     let inv_det = Vec4::splat(1.0) / det;
-
-    //     // XMMATRIX mResult;
-    //     // mResult.r[0] = _mm_mul_ps(C0,vTemp);
-    //     // mResult.r[1] = _mm_mul_ps(C2,vTemp);
-    //     // mResult.r[2] = _mm_mul_ps(C4,vTemp);
-    //     // mResult.r[3] = _mm_mul_ps(C6,vTemp);
-    //     // return mResult;
-    //     Some(Mat4 {
-    //         x_axis: c0 * inv_det,
-    //         y_axis: c2 * inv_det,
-    //         z_axis: c4 * inv_det,
-    //         w_axis: c6 * inv_det,
-    //     })
-    // }
-
-    // #[inline]
-    // pub fn inverse_rtm(&self) -> Option<Mat4> {
-    //     let transpose = self.transpose();
-    //     let tx = transpose.get_x_axis();
-    //     let ty = transpose.get_y_axis();
-    //     let tz = transpose.get_z_axis();
-    //     let tw = transpose.get_w_axis();
-
-    //     let mut v00 = tz.permute(0b00_00_01_01);
-    //     let mut v01 = tx.permute(0b00_00_01_01);
-    //     let mut v02 = tz.shuffle(tx, 0b00_10_00_10);
-    //     let mut v10 = tw.permute(0b10_11_10_11);
-    //     let mut v11 = ty.permute(0b10_11_10_11);
-    //     let mut v12 = tw.shuffle(ty, 0b01_11_01_11);
-
-    //     let mut d0 = v00.mul(v10);
-    //     let mut d1 = v01.mul(v11);
-    //     let mut d2 = v02.mul(v12);
-
-    //     v00 = tz.permute(0b10_11_10_11);
-    //     v01 = tx.permute(0b10_11_10_11);
-    //     v02 = tz.shuffle(tx, 0b01_11_01_11);
-    //     v10 = tw.permute(0b00_00_01_01);
-    //     v11 = ty.permute(0b00_00_01_01);
-    //     v12 = tw.shuffle(ty, 0b00_10_00_10);
-
-    //     d0 = v00.neg_mul_sub(v10, d0);
-    //     d1 = v01.neg_mul_sub(v11, d1);
-    //     d2 = v02.neg_mul_sub(v12, d2);
-
-    //     v00 = ty.permute(0b01_10_00_01);
-    //     v01 = tx.permute(0b10_00_01_00);
-    //     v02 = tw.permute(0b01_10_00_01);
-    //     let mut v03 = tz.permute(0b10_00_01_00);
-    //     v10 = vector_mix<mix4::b, mix4::y, mix4::w, mix4::x>(d0, d2);
-    //     v11 = vector_mix<mix4::w, mix4::b, mix4::y, mix4::z>(d0, d2);
-    //     v12 = vector_mix<mix4::d, mix4::y, mix4::w, mix4::x>(d1, d2);
-    //     let mut v13 = vector_mix<mix4::w, mix4::d, mix4::y, mix4::z>(d1, d2);
-
-    //     let mut c0 = v00.mul(v10);
-    //     let mut c2 = v01.mul(v11);
-    //     let mut c4 = v02.mul(v12);
-    //     let mut c6 = v03.mul(v13);
-
-    //     v00 = vector_mix<mix4::z, mix4::w, mix4::y, mix4::z>(ty, ty);
-    //     v01 = vector_mix<mix4::w, mix4::z, mix4::w, mix4::y>(tx, tx);
-    //     v02 = vector_mix<mix4::z, mix4::w, mix4::y, mix4::z>(tw, tw);
-    //     v03 = vector_mix<mix4::w, mix4::z, mix4::w, mix4::y>(tz, tz);
-    //     v10 = vector_mix<mix4::w, mix4::x, mix4::y, mix4::a>(d0, d2);
-    //     v11 = vector_mix<mix4::z, mix4::y, mix4::a, mix4::x>(d0, d2);
-    //     v12 = vector_mix<mix4::w, mix4::x, mix4::y, mix4::c>(d1, d2);
-    //     v13 = vector_mix<mix4::z, mix4::y, mix4::c, mix4::x>(d1, d2);
-
-    //     c0 = v00.neg_mul_sub(v10, c0);
-    //     c2 = v01.neg_mul_sub(v11, c2);
-    //     c4 = v02.neg_mul_sub(v12, c4);
-    //     c6 = v03.neg_mul_sub(v13, c6);
-
-    //     v00 = vector_mix<mix4::w, mix4::x, mix4::w, mix4::x>(ty, ty);
-    //     v01 = vector_mix<mix4::y, mix4::w, mix4::x, mix4::z>(tx, tx);
-    //     v02 = vector_mix<mix4::w, mix4::x, mix4::w, mix4::x>(tw, tw);
-    //     v03 = vector_mix<mix4::y, mix4::w, mix4::x, mix4::z>(tz, tz);
-    //     v10 = vector_mix<mix4::z, mix4::b, mix4::a, mix4::z>(d0, d2);
-    //     v11 = vector_mix<mix4::b, mix4::x, mix4::w, mix4::a>(d0, d2);
-    //     v12 = vector_mix<mix4::z, mix4::d, mix4::c, mix4::z>(d1, d2);
-    //     v13 = vector_mix<mix4::d, mix4::x, mix4::w, mix4::c>(d1, d2);
-
-    //     let c1 = v00.neg_mul_sub(v10, c0);
-    //     c0 = v00.mul_add(v10, c0);
-    //     let c3 = v01.mul_add(v11, c2);
-    //     c2 = v01.neg_mul_sub(v11, c2);
-    //     let c5 = v02.neg_mul_sub(v12, c4);
-    //     c4 = v02.mul_add(v12, c4);
-    //     let c7 = v03.mul_add(v13, c6);
-    //     c6 = v03.neg_mul_sub(v13, c6);
-
-    //     let mut x_axis = vector_mix<mix4::x, mix4::b, mix4::z, mix4::d>(c0, c1);
-    //     let mut y_axis = vector_mix<mix4::x, mix4::b, mix4::z, mix4::d>(c2, c3);
-    //     let mut z_axis = vector_mix<mix4::x, mix4::b, mix4::z, mix4::d>(c4, c5);
-    //     let mut w_axis = vector_mix<mix4::x, mix4::b, mix4::z, mix4::d>(c6, c7);
-
-    //     let det = vector_dot_as_scalar(x_axis, tx);
-    //     let inv_det = vector_set(scalar_reciprocal(det));
-
-    //     x_axis = x_axis.mul(inv_det);
-    //     y_axis = y_axis.mul(inv_det);
-    //     z_axis = z_axis.mul(inv_det);
-    //     w_axis = w_axis.mul(inv_det);
-
-    //     Mat4 { x_axis, y_axis, z_axis, w_axis }
-    // }
 
     #[inline]
     pub fn look_at(eye: Vec3, center: Vec3, up: Vec3) -> Mat4 {
