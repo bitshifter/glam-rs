5d8e72ad83f6a30fd76450b01f5c9191839eefac
Cameron Hart <cameron.hart@gmail.com>
2019-05-24 21:40:32 +1000

    Make union cast pub(crate)

--- src/f32/x86_utils.rs
+++ src/f32/x86_utils.rs
@@ -3,7 +3,7 @@ use std::arch::x86::*;
 #[cfg(target_arch = "x86_64")]
 use std::arch::x86_64::*;
 
-pub union UnionCast {
+pub(crate) union UnionCast {
     pub m128: __m128,
     pub m128i: __m128i,
     pub f32x4: [f32; 4],
