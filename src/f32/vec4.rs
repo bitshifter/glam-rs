076403b8f9db55c77f7167adb651740a05cc2076
Nathan Vegdahl <cessen@cessen.com>
2019-07-21 15:20:27 +0900

    Method documentation for `Vec4`.

--- src/f32/vec4.rs
+++ src/f32/vec4.rs
@@ -9,24 +9,36 @@ pub fn vec4(x: f32, y: f32, z: f32, w: f32) -> Vec4 {
 }
 
 impl Vec4 {
+    /// Returns a new `Vec4` with `1.0` for elements that are positive in
+    /// the original and `-1.0` for elements that are negative.
     #[inline]
     pub fn sign(self) -> Self {
         let mask = self.cmpge(Self::zero());
         mask.select(Self::splat(1.0), Self::splat(-1.0))
     }
 
+    /// Computes the reciprocal (`1.0/n`) of each element, returning the
+    /// results in a new `Vec4`.
     #[inline]
     pub fn reciprocal(self) -> Self {
         // TODO: Optimize
         Self::one() / self
     }
 
+    /// Performs a linear interpolation between the `Vec4` and `rhs` based on
+    /// the value `s`.
+    ///
+    /// When `s` is `0.0`, the result will be equal to the `Vec4`.  When `s`
+    /// is `1.0`, the result will be equal to `rhs`.
     #[inline]
     pub fn lerp(self, rhs: Self, s: f32) -> Self {
         glam_assert!(s >= 0.0 && s <= 1.0);
         self + ((rhs - self) * s)
     }
 
+    /// Returns whether the `Vec4` is normalized to length `1.0` or not.
+    ///
+    /// Uses a precision threshold of `0.00001`.
     #[inline]
     pub fn is_normalized(self) -> bool {
         is_normalized!(self)
