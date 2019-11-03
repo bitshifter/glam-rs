da8982332f5c51e29dd5a1b3f2f44c38b40cdae8
Cameron Hart <cameron.hart@gmail.com>
2019-11-03 21:48:21 +1100

    Fix warning.

--- src/f32/x86_utils.rs
+++ src/f32/x86_utils.rs
@@ -3,6 +3,7 @@ use std::arch::x86::*;
 #[cfg(target_arch = "x86_64")]
 use std::arch::x86_64::*;
 
+#[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
 pub(crate) union UnionCast {
     pub m128: __m128,
     pub m128i: __m128i,
