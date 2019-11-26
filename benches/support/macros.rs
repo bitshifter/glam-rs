f0f760e32decb131f067c98921111ce9f9793101
Cameron Hart <cameron.hart@gmail.com>
2019-11-26 22:59:36 +1100

    Switched benchmarks back to `iter`.
    Same as mathbench. Too many weird results with `iter_batched`.

--- benches/support/macros.rs
+++ benches/support/macros.rs
@@ -2,14 +2,22 @@
 macro_rules! bench_func {
     ($name: ident, $desc: expr, op => $func: ident, from => $from: expr) => {
         pub(crate) fn $name(c: &mut Criterion) {
+            const SIZE: usize = 1 << 13;
             let mut rng = support::PCG32::default();
+            let inputs =
+                criterion::black_box((0..SIZE).map(|_| $from(&mut rng)).collect::<Vec<_>>());
+            // pre-fill output vector with some random value
+            let mut outputs = vec![$func($from(&mut rng)); SIZE];
+            let mut i = 0;
             c.bench_function($desc, |b| {
-                b.iter_batched(
-                    || $from(&mut rng),
-                    |data| $func(data),
-                    criterion::BatchSize::SmallInput,
-                )
+                b.iter(|| {
+                    i = (i + 1) & (SIZE - 1);
+                    unsafe {
+                        *outputs.get_unchecked_mut(i) = $func(*inputs.get_unchecked(i));
+                    }
+                })
             });
+            criterion::black_box(outputs);
         }
     };
 }
@@ -18,14 +26,22 @@ macro_rules! bench_func {
 macro_rules! bench_unop {
     ($name: ident, $desc: expr, op => $unop: ident, from => $from: expr) => {
         pub(crate) fn $name(c: &mut Criterion) {
+            const SIZE: usize = 1 << 13;
             let mut rng = support::PCG32::default();
+            let inputs =
+                criterion::black_box((0..SIZE).map(|_| $from(&mut rng)).collect::<Vec<_>>());
+            // pre-fill output vector with some random value
+            let mut outputs = vec![$from(&mut rng).$unop(); SIZE];
+            let mut i = 0;
             c.bench_function($desc, |b| {
-                b.iter_batched(
-                    || $from(&mut rng),
-                    |data| data.$unop(),
-                    criterion::BatchSize::SmallInput,
-                )
+                b.iter(|| {
+                    i = (i + 1) & (SIZE - 1);
+                    unsafe {
+                        *outputs.get_unchecked_mut(i) = inputs.get_unchecked(i).$unop();
+                    }
+                })
             });
+            criterion::black_box(outputs);
         }
     };
 }
@@ -34,14 +50,24 @@ macro_rules! bench_unop {
 macro_rules! bench_binop {
     ($name: ident, $desc: expr, op => $binop: ident, from1 => $from1:expr, from2 => $from2:expr) => {
         pub(crate) fn $name(c: &mut Criterion) {
+            const SIZE: usize = 1 << 13;
             let mut rng = support::PCG32::default();
+            let inputs1 =
+                criterion::black_box((0..SIZE).map(|_| $from1(&mut rng)).collect::<Vec<_>>());
+            let inputs2 =
+                criterion::black_box((0..SIZE).map(|_| $from2(&mut rng)).collect::<Vec<_>>());
+            // pre-fill output vector with some random value
+            let mut outputs = vec![$from1(&mut rng).$binop($from2(&mut rng)); SIZE];
+            let mut i = 0;
             c.bench_function($desc, |b| {
-                b.iter_batched(
-                    || ($from1(&mut rng), $from2(&mut rng)),
-                    |data| (data.0).$binop(data.1),
-                    criterion::BatchSize::SmallInput,
-                )
+                b.iter(|| {
+                    i = (i + 1) & (SIZE - 1);
+                    unsafe {
+                        *outputs.get_unchecked_mut(i) = inputs1.get_unchecked(i).$binop(*inputs2.get_unchecked(i));
+                    }
+                })
             });
+            criterion::black_box(outputs);
         }
     };
     ($name: ident, $desc: expr, op => $binop: ident, from => $from: expr) => {
@@ -53,14 +79,29 @@ macro_rules! bench_binop {
 macro_rules! bench_trinop {
     ($name: ident, $desc: expr, op => $trinop: ident, from1 => $from1:expr, from2 => $from2:expr, from3 => $from3:expr) => {
         pub(crate) fn $name(c: &mut Criterion) {
+            const SIZE: usize = 1 << 13;
             let mut rng = support::PCG32::default();
+            let inputs1 =
+                criterion::black_box((0..SIZE).map(|_| $from1(&mut rng)).collect::<Vec<_>>());
+            let inputs2 =
+                criterion::black_box((0..SIZE).map(|_| $from2(&mut rng)).collect::<Vec<_>>());
+            let inputs3 =
+                criterion::black_box((0..SIZE).map(|_| $from3(&mut rng)).collect::<Vec<_>>());
+            // pre-fill output vector with some random value
+            let mut outputs =
+                vec![$from1(&mut rng).$trinop($from2(&mut rng), $from3(&mut rng)); SIZE];
+            let mut i = 0;
             c.bench_function($desc, |b| {
-                b.iter_batched(
-                    || ($from1(&mut rng), $from2(&mut rng), $from3(&mut rng)),
-                    |data| (data.0).$trinop(data.1, data.2),
-                    criterion::BatchSize::SmallInput,
-                )
+                b.iter(|| {
+                    i = (i + 1) & (SIZE - 1);
+                    unsafe {
+                        *outputs.get_unchecked_mut(i) = inputs1
+                            .get_unchecked(i)
+                            .$trinop(*inputs2.get_unchecked(i), *inputs3.get_unchecked(i));
+                    }
+                })
             });
+            criterion::black_box(outputs);
         }
     };
 }
@@ -69,19 +110,30 @@ macro_rules! bench_trinop {
 macro_rules! bench_from_ypr {
     ($name: ident, $desc: expr, ty => $ty:ty) => {
         pub(crate) fn $name(c: &mut Criterion) {
+            const SIZE: usize = 1 << 13;
             let mut rng = support::PCG32::default();
-            c.bench_function($desc, move |b| {
-                b.iter_batched(
-                    || {
+            let inputs = criterion::black_box(
+                (0..SIZE)
+                    .map(|_| {
                         (
                             random_radians(&mut rng),
                             random_radians(&mut rng),
                             random_radians(&mut rng),
                         )
-                    },
-                    |data| <$ty>::from_rotation_ypr(data.0, data.1, data.2),
-                    criterion::BatchSize::SmallInput,
-                )
+                    })
+                    .collect::<Vec<_>>(),
+            );
+            let mut outputs = vec![<$ty>::default(); SIZE];
+            let mut i = 0;
+            c.bench_function($desc, move |b| {
+                b.iter(|| {
+                    i = (i + 1) & (SIZE - 1);
+                    unsafe {
+                        let data = inputs.get_unchecked(i);
+                        *outputs.get_unchecked_mut(i) =
+                            <$ty>::from_rotation_ypr(data.0, data.1, data.2)
+                    }
+                })
             });
         }
     };
@@ -95,39 +147,33 @@ macro_rules! euler {
             const NUM_OBJECTS: usize = 10000;
 
             struct TestData {
-                acceleration: Vec<$storage>,
-                velocity: Vec<$storage>,
-                position: Vec<$storage>,
+                acc: Vec<$storage>,
+                vel: Vec<$storage>,
+                pos: Vec<$storage>,
             }
 
             let mut rng = support::PCG32::default();
+            let mut data = TestData {
+                acc: vec![$rand(&mut rng); NUM_OBJECTS],
+                vel: vec![$zero; NUM_OBJECTS],
+                pos: vec![$zero; NUM_OBJECTS],
+            };
             let dt = <$t>::splat(UPDATE_RATE);
 
             c.bench_function($desc, move |b| {
-                b.iter_batched_ref(
-                    || TestData {
-                        acceleration: vec![$rand(&mut rng); NUM_OBJECTS],
-                        velocity: vec![<$t>::zero(); NUM_OBJECTS],
-                        position: vec![<$t>::zero(); NUM_OBJECTS],
-                    },
-                    |data| {
-                        for ((position, acceleration), velocity) in data
-                            .position
-                            .iter_mut()
-                            .zip(&data.acceleration)
-                            .zip(&mut data.velocity)
-                        {
-                            let local_acc: $t = (*acceleration).into();
-                            let mut local_pos: $t = (*position).into();
-                            let mut local_vel: $t = (*velocity).into();
-                            local_vel += local_acc * dt;
-                            local_pos += local_vel * dt;
-                            *velocity = local_vel.into();
-                            *position = local_pos.into();
-                        }
-                    },
-                    criterion::BatchSize::SmallInput,
-                )
+                b.iter(|| {
+                    for ((position, acceleration), velocity) in
+                        data.pos.iter_mut().zip(&data.acc).zip(&mut data.vel)
+                    {
+                        let local_acc: $t = (*acceleration).into();
+                        let mut local_pos: $t = (*position).into();
+                        let mut local_vel: $t = (*velocity).into();
+                        local_vel += local_acc * dt;
+                        local_pos += local_vel * dt;
+                        *velocity = local_vel.into();
+                        *position = local_pos.into();
+                    }
+                })
             });
         }
     };
