9153d4f62ba567cd49a92491d4c2fb764431e20e
michal-z <michalziulek@gmail.com>
2019-12-26 23:45:23 +0100

    m128_round() founction needs sse2.

--- src/f32/funcs.rs
+++ src/f32/funcs.rs
@@ -50,9 +50,10 @@ pub(crate) fn scalar_sin_cos(x: f32) -> (f32, f32) {
     x.sin_cos()
 }
 
-// From DirectXMath.
+#[cfg(all(target_feature = "sse2", not(feature = "scalar-math")))]
 #[inline]
 pub unsafe fn m128_round(v: __m128) -> __m128 {
+    // From DirectXMath XMVectorRound.
     let sign = _mm_and_ps(v, PS_SIGN_MASK.m128);
     let s_magic = _mm_or_ps(PS_NO_FRACTION.m128, sign);
     let r1 = _mm_add_ps(v, s_magic);
