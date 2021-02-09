9fdf0ed49ad524ed09cf4cf5cbd30f5a7b6c3c4b
Emil Ernerfeldt <emil.ernerfeldt@gmail.com>
2021-02-09 11:08:23 +0100

    Add Vec::normalize_or_zero() (#127)
    This adds the method `normalize_or_zero()` which will either
    return a unit-length vector or zero, but never a non-finite vector.

--- src/vec.rs
+++ src/vec.rs
@@ -246,10 +246,25 @@ macro_rules! impl_vecn_float_methods {
 
         /// Returns `self` normalized to length 1.0.
         ///
-        /// For valid results, `self` must _not_ be of length zero.
+        /// For valid results, `self` must _not_ be of length zero, nor very close to zero.
+        /// You can check the results with [`Self::is_finite`], or use [`Self::normalize_or_zero`].
         #[inline(always)]
         pub fn normalize(self) -> Self {
             Self($flttrait::normalize(self.0))
+        }
+
+        /// Returns `self` normalized to length 1.0 if possible, else returns zero.
+        ///
+        /// In particular, if the input is zero (or very close to zero), or non-finite,
+        /// the result of this operation will be zero.
+        #[inline(always)]
+        pub fn normalize_or_zero(self) -> Self {
+            let rcp = self.length_recip();
+            if rcp.is_finite() && rcp > 0.0 {
+                self * rcp
+            } else {
+                Self::ZERO
+            }
         }
 
         /// Returns whether `self` is length `1.0` or not.
--- tests/support/macros.rs
+++ tests/support/macros.rs
@@ -30,3 +30,56 @@ macro_rules! assert_approx_eq {
         );
     }};
 }
+
+/// Test vector normalization for float vector
+#[macro_export]
+macro_rules! impl_vec_float_normalize_tests {
+    ($t:ident, $vec:ident) => {
+        use core::$t::MAX;
+        use core::$t::MIN_POSITIVE;
+
+        /// Works for vec2, vec3, vec4
+        fn from_x_y(x: $t, y: $t) -> $vec {
+            let mut v = $vec::ZERO;
+            v.x = x;
+            v.y = y;
+            v
+        }
+
+        #[test]
+        fn test_normalize() {
+            assert_eq!(from_x_y(-42.0, 0.0).normalize(), from_x_y(-1.0, 0.0));
+            assert_eq!(from_x_y(MAX.sqrt(), 0.0).normalize(), from_x_y(1.0, 0.0));
+            // assert_eq!(from_x_y(MAX, 0.0).normalize(), from_x_y(1.0, 0.0)); // normalize fails for huge vectors and returns zero
+
+            // We expect not to be able to normalize small numbers:
+            assert!(!from_x_y(0.0, 0.0).normalize().is_finite());
+            assert!(!from_x_y(MIN_POSITIVE, 0.0).normalize().is_finite());
+
+            // We expect not to be able to normalize non-finite vectors:
+            assert!(!from_x_y(INFINITY, 0.0).normalize().is_finite());
+            assert!(!from_x_y(NAN, 0.0).normalize().is_finite());
+        }
+
+        #[test]
+        fn test_normalize_or_zero() {
+            assert_eq!(from_x_y(-42.0, 0.0).normalize(), from_x_y(-1.0, 0.0));
+            assert_eq!(
+                from_x_y(MAX.sqrt(), 0.0).normalize_or_zero(),
+                from_x_y(1.0, 0.0)
+            );
+
+            // We expect `normalize_or_zero` to return zero when inputs are very small:
+            assert_eq!(from_x_y(0.0, 0.0).normalize_or_zero(), $vec::ZERO);
+            assert_eq!(from_x_y(MIN_POSITIVE, 0.0).normalize_or_zero(), $vec::ZERO);
+
+            // We expect `normalize_or_zero` to return zero when inputs are non-finite:
+            assert_eq!(from_x_y(INFINITY, 0.0).normalize_or_zero(), $vec::ZERO);
+            assert_eq!(from_x_y(NAN, 0.0).normalize_or_zero(), $vec::ZERO);
+
+            // We expect `normalize_or_zero` to return zero when inputs are very large:
+            assert_eq!(from_x_y(MAX, 0.0).normalize_or_zero(), $vec::ZERO);
+            assert_eq!(from_x_y(MAX, MAX).normalize_or_zero(), $vec::ZERO);
+        }
+    };
+}
