b3096cb92b8bdc34fc700f9c2de073853fe675dd
Emil Ernerfeldt <emil.ernerfeldt@gmail.com>
2021-04-07 15:35:21 +0200

    Remove duplicated benchmark

--- benches/vec4.rs
+++ benches/vec4.rs
@@ -6,15 +6,7 @@ mod support;
 use criterion::{criterion_group, criterion_main, Criterion};
 use glam::Vec4;
 use std::ops::Mul;
-use support::{random_srt_mat4, random_vec4};
-
-bench_binop!(
-    vec4_mul_mat4,
-    "vec4 mul mat4",
-    op => mul,
-    from1 => random_srt_mat4,
-    from2 => random_vec4
-);
+use support::random_vec4;
 
 bench_select!(
     vec4_select,
