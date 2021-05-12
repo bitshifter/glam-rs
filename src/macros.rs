a0b9014adfb5df5df40caa4360d247aefe15cc2a
Cameron Hart <cameron.hart@gmail.com>
2021-05-13 10:36:57 +1200

    clippy fix

--- src/macros.rs
+++ src/macros.rs
@@ -15,7 +15,7 @@ macro_rules! glam_assert {
 
 macro_rules! const_assert {
     ($x:expr $(,)?) => {
-        #[allow(unknown_lints, eq_op)]
+        #[allow(unknown_lints, clippy::eq_op)]
         const _: [(); 0 - !{
             const ASSERT: bool = $x;
             ASSERT
