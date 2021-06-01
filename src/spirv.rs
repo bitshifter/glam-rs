da1071fab435ab2755ea6acf99ffe642b4689f7b
khyperia <github@khyperia.com>
2021-06-01 12:49:32 +0200

    Remove the spirv-std dependency

--- src/spirv.rs
+++ src/spirv.rs
@@ -24,26 +24,3 @@ unsupported_features! {
     "serde",
     "std",
 }
-
-use spirv_std::vector::Vector;
-
-unsafe impl Vector<bool, 2> for crate::BVec2 {}
-unsafe impl Vector<bool, 3> for crate::BVec3 {}
-unsafe impl Vector<bool, 4> for crate::BVec4 {}
-
-unsafe impl Vector<f32, 2> for crate::Vec2 {}
-unsafe impl Vector<f32, 3> for crate::Vec3 {}
-unsafe impl Vector<f32, 3> for crate::Vec3A {}
-unsafe impl Vector<f32, 4> for crate::Vec4 {}
-
-unsafe impl Vector<f64, 2> for crate::DVec2 {}
-unsafe impl Vector<f64, 3> for crate::DVec3 {}
-unsafe impl Vector<f64, 4> for crate::DVec4 {}
-
-unsafe impl Vector<u32, 2> for crate::UVec2 {}
-unsafe impl Vector<u32, 3> for crate::UVec3 {}
-unsafe impl Vector<u32, 4> for crate::UVec4 {}
-
-unsafe impl Vector<i32, 2> for crate::IVec2 {}
-unsafe impl Vector<i32, 3> for crate::IVec3 {}
-unsafe impl Vector<i32, 4> for crate::IVec4 {}
