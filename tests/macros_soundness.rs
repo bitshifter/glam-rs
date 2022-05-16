5eff86ebc804a986d08ce0317aadd1597abe05bf
Giacomo Stevanato <giaco.stevanato@gmail.com>
2022-05-16 22:26:48 +0200

    Remove failing test

--- tests/macros_soundness.rs
+++
@@ -1,6 +0,0 @@
-#[test]
-fn check_macros_soundness() {
-    let t = trybuild::TestCases::new();
-    t.pass("tests/macros_soundness/should_compile.rs");
-    t.compile_fail("tests/macros_soundness/should_fail.rs");
-}
