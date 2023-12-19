2d527b96e54e58af76070dc9f43ecedc667395a7
Cameron Hart <cameron.hart@gmail.com>
2023-12-19 14:22:09 +1300

    Prepare for release 0.25.0 (#451)

--- src/lib.rs
+++ src/lib.rs
@@ -251,7 +251,7 @@ and benchmarks.
 The minimum supported Rust version is `1.58.1`.
 
 */
-#![doc(html_root_url = "https://docs.rs/glam/0.24.2")]
+#![doc(html_root_url = "https://docs.rs/glam/0.25.0")]
 #![cfg_attr(not(feature = "std"), no_std)]
 #![cfg_attr(target_arch = "spirv", feature(repr_simd))]
 #![deny(
