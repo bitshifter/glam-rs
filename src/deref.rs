c95f107983926a1ecdf1051ddc4d60f16f28bea2
Cameron Hart <cameron.hart@gmail.com>
2024-06-04 23:31:03 +1200

    Remove derives from deref types as these had unintended uses. (#520)
    Fixes #518

--- src/deref.rs
+++ src/deref.rs
@@ -1,12 +1,4 @@
-#[derive(Clone, Copy, Debug, Default, PartialEq, Eq, PartialOrd)]
-#[cfg_attr(target_arch = "spirv", repr(simd))]
-#[cfg_attr(not(target_arch = "spirv"), repr(C))]
-pub struct XY<T> {
-    pub x: T,
-    pub y: T,
-}
-
-#[derive(Clone, Copy, Debug, Default, PartialEq, Eq, PartialOrd)]
+#[cfg(not(feature = "scalar-math"))]
 #[cfg_attr(target_arch = "spirv", repr(simd))]
 #[cfg_attr(not(target_arch = "spirv"), repr(C))]
 pub struct Vec3<T> {
@@ -15,7 +7,7 @@ pub struct Vec3<T> {
     pub z: T,
 }
 
-#[derive(Clone, Copy, Debug, Default, PartialEq, Eq, PartialOrd)]
+#[cfg(not(feature = "scalar-math"))]
 #[cfg_attr(target_arch = "spirv", repr(simd))]
 #[cfg_attr(not(target_arch = "spirv"), repr(C))]
 pub struct Vec4<T> {
@@ -25,14 +17,13 @@ pub struct Vec4<T> {
     pub w: T,
 }
 
-#[derive(Clone, Copy, Default, PartialEq, Eq, PartialOrd)]
+#[cfg(not(feature = "scalar-math"))]
 #[cfg_attr(not(target_arch = "spirv"), repr(C))]
 pub struct Cols2<V> {
     pub x_axis: V,
     pub y_axis: V,
 }
 
-#[derive(Clone, Copy, Default, PartialEq, Eq, PartialOrd)]
 #[cfg_attr(not(target_arch = "spirv"), repr(C))]
 pub struct Cols3<V> {
     pub x_axis: V,
@@ -40,7 +31,6 @@ pub struct Cols3<V> {
     pub z_axis: V,
 }
 
-#[derive(Clone, Copy, Default, PartialEq, Eq, PartialOrd)]
 #[cfg_attr(not(target_arch = "spirv"), repr(C))]
 pub struct Cols4<V> {
     pub x_axis: V,
