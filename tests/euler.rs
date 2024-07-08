530a0f6a1e355eff74a4206f3feeea18285582fc
Cameron Hart <cameron.hart@gmail.com>
2024-07-09 00:08:50 +1200

    Add more test coverage.

--- tests/euler.rs
+++ tests/euler.rs
@@ -101,16 +101,134 @@ macro_rules! impl_all_quat_tests_three_axis {
 mod euler {
     use super::{CanonicalQuat, EulerEpsilon};
     use glam::*;
-    type ER = EulerRot;
+
+    // fn test_random_angles<F: Fn(&Mat4, &Fn(&Mat4, EulerRot) -> Mat4, EulerRot) -> Mat4>(rot_euler_rot: &F, order: EulerRot)
+    // {
+    //     use rand::{Rng, SeedableRng};
+    //     use rand_xoshiro::Xoshiro256Plus;
+    //     let mut rng = Xoshiro256Plus::seed_from_u64(0);
+    //     let i = rng.gen_range(-f32::consts::PI, f32::consts::PI);
+    //     let j = rng.gen_range(-f32::consts::PI, f32::consts::PI);
+    //     let k = rng.gen_range(-f32::consts::PI, f32::consts::PI);
+    //     for _ in 0..100000 {
+    //         let m = Mat4::from_euler(order, i, j, k);
+
+    //         // Add a small random error to the elements of m
+    //         for c in 0..3 {
+    //             let col = m.col_mut(c);
+    //             for r in 0..3 {
+    //                 col[r] = rng.get_range(-1e-7, 1e-7);
+    //             }
+    //         }
+
+    //         // Extract Euler angles from m, convert the Euler angles back to a matrix n and verify that
+    //         // the entries in m and n do not differ too much.
+    //         test_matrix(m, &matrix_euler_matrix, order);
+    //     }
+    // }
+
+    fn test_mat4_euler_mat4(order: EulerRot, a: i32, b: i32, c: i32) {
+        println!("test_mat4_euler_mat4: {order:?} ({a}, {b}, {c})");
+        let a = (a as f32).to_radians();
+        let b = (b as f32).to_radians();
+        let c = (c as f32).to_radians();
+        let m = Mat4::from_euler(order, a, b, c);
+        let (i, j, k) = m.to_euler(order);
+        let n = Mat4::from_euler(order, i, j, k);
+        assert_approx_eq!(m, n, 2e-6);
+    }
+
+    fn test_mat3_euler_mat3(order: EulerRot, a: i32, b: i32, c: i32) {
+        println!("test_mat3_euler_mat3: {order:?} ({a}, {b}, {c})");
+        let a = (a as f32).to_radians();
+        let b = (b as f32).to_radians();
+        let c = (c as f32).to_radians();
+        let m = Mat3::from_euler(order, a, b, c);
+        let (i, j, k) = m.to_euler(order);
+        let n = Mat3::from_euler(order, i, j, k);
+        assert_approx_eq!(m, n, 2e-6);
+    }
+
+    fn test_quat_euler_quat(order: EulerRot, a: i32, b: i32, c: i32) {
+        println!("test_quat_euler_quat: {order:?} ({a}, {b}, {c})");
+        let a = (a as f32).to_radians();
+        let b = (b as f32).to_radians();
+        let c = (c as f32).to_radians();
+        let m = Quat::from_euler(order, a, b, c);
+        let (i, j, k) = m.to_euler(order);
+        let n = Quat::from_euler(order, i, j, k);
+        assert_approx_eq!(m.canonical(), n.canonical(), 2e-6);
+    }
+
+    fn test_order_angles<F: Fn(EulerRot, i32, i32, i32)>(order: EulerRot, test: &F) {
+        const STEP: usize = 90;
+        for i in (0..360).step_by(STEP) {
+            for j in (0..360).step_by(STEP) {
+                for k in (0..360).step_by(STEP) {
+                    test(order, i, j, k);
+                }
+            }
+        }
+    }
+
+    fn test_all_orders<F: Fn(EulerRot)>(test: &F) {
+        test(EulerRot::XYZ);
+        test(EulerRot::XZY);
+        test(EulerRot::YZX);
+        test(EulerRot::YXZ);
+        test(EulerRot::ZXY);
+        test(EulerRot::ZYX);
+
+        test(EulerRot::XZX);
+        test(EulerRot::XYX);
+        test(EulerRot::YXY);
+        test(EulerRot::YZY);
+        test(EulerRot::ZYZ);
+        test(EulerRot::ZXZ);
+
+        test(EulerRot::XYZEx);
+        test(EulerRot::XZYEx);
+        test(EulerRot::YZXEx);
+        test(EulerRot::YXZEx);
+        test(EulerRot::ZXYEx);
+        test(EulerRot::ZYXEx);
+
+        test(EulerRot::XZXEx);
+        test(EulerRot::XYXEx);
+        test(EulerRot::YXYEx);
+        test(EulerRot::YZYEx);
+        test(EulerRot::ZYZEx);
+        test(EulerRot::ZXZEx);
+    }
+
+    #[test]
+    fn test_all_mat4_euler_orders() {
+        let test = |order| test_order_angles(order, &test_mat4_euler_mat4);
+        test_all_orders(&test);
+    }
+
+    #[test]
+    fn test_all_mat3_euler_orders() {
+        let test = |order| test_order_angles(order, &test_mat3_euler_mat3);
+        test_all_orders(&test);
+    }
+
+    #[test]
+    fn test_all_quat_euler_orders() {
+        let test = |order| test_order_angles(order, &test_quat_euler_quat);
+        test_all_orders(&test);
+    }
 
     mod quat {
         use super::*;
+        type ER = EulerRot;
 
         impl_all_quat_tests_three_axis!(f32, Quat, Vec3);
     }
 
     mod dquat {
         use super::*;
+        type ER = EulerRot;
 
         impl_all_quat_tests_three_axis!(f64, DQuat, DVec3);
     }
