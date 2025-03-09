0cb117a3d6dde0807373176b1cb0e0691bac96ec
Cameron Hart <cameron.hart@gmail.com>
2025-03-09 23:51:55 +1300

    mint and serde implementation for usize vector types.

--- src/features/impl_mint.rs
+++ src/features/impl_mint.rs
@@ -3,8 +3,8 @@ use mint::IntoMint;
 use crate::{
     DMat2, DMat3, DMat4, DQuat, DVec2, DVec3, DVec4, I16Vec2, I16Vec3, I16Vec4, I64Vec2, I64Vec3,
     I64Vec4, I8Vec2, I8Vec3, I8Vec4, IVec2, IVec3, IVec4, Mat2, Mat3, Mat3A, Mat4, Quat, U16Vec2,
-    U16Vec3, U16Vec4, U64Vec2, U64Vec3, U64Vec4, U8Vec2, U8Vec3, U8Vec4, UVec2, UVec3, UVec4, Vec2,
-    Vec3, Vec3A, Vec4,
+    U16Vec3, U16Vec4, U64Vec2, U64Vec3, U64Vec4, U8Vec2, U8Vec3, U8Vec4, USizeVec2, USizeVec3,
+    USizeVec4, UVec2, UVec3, UVec4, Vec2, Vec3, Vec3A, Vec4,
 };
 
 macro_rules! impl_vec_types {
@@ -319,6 +319,7 @@ impl_vec_types!(i32, IVec2, IVec3, IVec4);
 impl_vec_types!(u32, UVec2, UVec3, UVec4);
 impl_vec_types!(i64, I64Vec2, I64Vec3, I64Vec4);
 impl_vec_types!(u64, U64Vec2, U64Vec3, U64Vec4);
+impl_vec_types!(usize, USizeVec2, USizeVec3, USizeVec4);
 
 #[cfg(test)]
 mod test {
