eff71b6da61fbf519053ac7a1117289de3815f85
Cameron Hart <cameron.hart@gmail.com>
2025-03-14 22:23:56 +1300

    Add a few more tests.

--- tests/vec3.rs
+++ tests/vec3.rs
@@ -1342,6 +1342,8 @@ macro_rules! impl_vec3_float_tests {
 
             // Self
             assert_approx_eq!($vec3::X, $vec3::X.rotate_towards($vec3::X, PI / 2.));
+            assert_approx_eq!($vec3::Y * 2.0, ($vec3::Y * 2.0).rotate_towards($vec3::Y, PI / 2.));
+            assert_approx_eq!($vec3::Z, $vec3::Z.rotate_towards($vec3::Z, PI / 2.));
 
             // Positive angle
             assert_approx_eq!($vec3::X, $vec3::X.rotate_towards($vec3::NEG_Y, 0.0));
