36c4942a3e67895d5ff4a20970dbb3335155687b
Cameron Hart <cameron.hart@gmail.com>
2025-09-15 23:23:14 +1200

    Test all encase impls.

--- src/features/impl_encase.rs
+++ src/features/impl_encase.rs
@@ -61,57 +61,97 @@ impl_matrix!(3, 3, Mat3, f32);
 impl_matrix!(4, 4, Mat4, f32);
 
 #[cfg(test)]
+macro_rules! impl_vec_test {
+    ($dim:literal, $test:ident, $ty:ty, $value:expr) => {
+        #[test]
+        fn $test() {
+            let v = <$ty>::from_array($value);
+
+            let mut buf = [255u8; $dim * 4];
+            let mut s_buf = StorageBuffer::new(&mut buf);
+
+            s_buf.write(&v).unwrap();
+            for i in 0..$dim {
+                assert_eq!(&s_buf.as_ref()[(i * 4)..][..4], &v[i].to_le_bytes());
+            }
+
+            let mut v_out = <$ty>::ZERO;
+            s_buf.read(&mut v_out).unwrap();
+            assert_eq!(v, v_out);
+
+            assert_eq!(v, s_buf.create().unwrap());
+        }
+    };
+}
+
+#[cfg(test)]
+macro_rules! impl_mat_test {
+    ($dim:literal, $stride:literal, $test:ident, $ty:ty, $value:expr) => {
+        #[test]
+        fn $test() {
+            let m = <$ty>::from_cols_array_2d(&$value);
+
+            let mut buf = [255u8; $dim * $stride * 4];
+            let mut s_buf = StorageBuffer::new(&mut buf);
+
+            s_buf.write(&m).unwrap();
+            for i in 0..$dim {
+                for j in 0..$dim {
+                    let stride = j * $stride * 4;
+                    let offset = i * 4;
+                    assert_eq!(
+                        &s_buf.as_ref()[(stride + offset)..][..4],
+                        &m.col(j)[i].to_le_bytes()
+                    );
+                }
+            }
+
+            let mut m_out = <$ty>::ZERO;
+            s_buf.read(&mut m_out).unwrap();
+            assert_eq!(m, m_out);
+
+            assert_eq!(m, s_buf.create().unwrap());
+        }
+    };
+}
+
+#[cfg(test)]
 mod test {
-    use crate::{Mat3, Vec3};
+    use crate::{IVec2, IVec3, IVec4, Mat2, Mat3, Mat4, UVec2, UVec3, UVec4, Vec2, Vec3, Vec4};
     use encase::StorageBuffer;
 
-    #[test]
-    fn vec3() {
-        let v = Vec3::new(1.12, 3.04, 6.75);
+    impl_vec_test!(2, vec2, Vec2, [1.12, 3.04]);
+    impl_vec_test!(2, ivec2, IVec2, [1, 3]);
+    impl_vec_test!(2, uvec2, UVec2, [1, 3]);
 
-        let mut buf = [255u8; 12];
-        let mut s_buf = StorageBuffer::new(&mut buf);
+    impl_vec_test!(3, vec3, Vec3, [1.12, 3.04, 6.75]);
+    impl_vec_test!(3, ivec3, IVec3, [1, 3, 6]);
+    impl_vec_test!(3, uvec3, UVec3, [1, 3, 6]);
 
-        s_buf.write(&v).unwrap();
-        assert_eq!(&s_buf.as_ref()[0..][..4], &v.x.to_le_bytes());
-        assert_eq!(&s_buf.as_ref()[4..][..4], &v.y.to_le_bytes());
-        assert_eq!(&s_buf.as_ref()[8..][..4], &v.z.to_le_bytes());
+    impl_vec_test!(4, vec4, Vec4, [1.12, 3.04, 6.75, 9.99]);
+    impl_vec_test!(4, ivec4, IVec4, [1, 3, 6, 9]);
+    impl_vec_test!(4, uvec4, UVec4, [1, 3, 6, 9]);
 
-        let mut v_out = Vec3::ZERO;
-        s_buf.read(&mut v_out).unwrap();
-        assert_eq!(v, v_out);
+    impl_mat_test!(2, 2, mat2, Mat2, [[1.12, 3.04], [8.65, 2.34]]);
 
-        assert_eq!(v, s_buf.create().unwrap());
-    }
+    impl_mat_test!(
+        3,
+        4,
+        mat3,
+        Mat3,
+        [[1.12, 3.04, 6.75], [8.65, 2.34, 94.6], [5.45, 6.34, 89.41]]
+    );
 
-    #[test]
-    fn mat3() {
-        let m = Mat3::from_cols(
-            [1.12, 3.04, 6.75].into(),
-            [8.65, 2.34, 94.6].into(),
-            [5.45, 6.34, 89.41].into(),
-        );
-
-        let mut buf = [255u8; 48];
-        let mut s_buf = StorageBuffer::new(&mut buf);
-
-        s_buf.write(&m).unwrap();
-        assert_eq!(&s_buf.as_ref()[0..][..4], &m.x_axis.x.to_le_bytes());
-        assert_eq!(&s_buf.as_ref()[4..][..4], &m.x_axis.y.to_le_bytes());
-        assert_eq!(&s_buf.as_ref()[8..][..4], &m.x_axis.z.to_le_bytes());
-
-        assert_eq!(&s_buf.as_ref()[16..][..4], &m.y_axis.x.to_le_bytes());
-        assert_eq!(&s_buf.as_ref()[20..][..4], &m.y_axis.y.to_le_bytes());
-        assert_eq!(&s_buf.as_ref()[24..][..4], &m.y_axis.z.to_le_bytes());
-
-        assert_eq!(&s_buf.as_ref()[32..][..4], &m.z_axis.x.to_le_bytes());
-        assert_eq!(&s_buf.as_ref()[36..][..4], &m.z_axis.y.to_le_bytes());
-        assert_eq!(&s_buf.as_ref()[40..][..4], &m.z_axis.z.to_le_bytes());
-
-        let mut m_out = Mat3::ZERO;
-        s_buf.read(&mut m_out).unwrap();
-        assert_eq!(m, m_out);
-
-        assert_eq!(m, s_buf.create().unwrap());
-    }
+    impl_mat_test!(
+        4,
+        4,
+        mat4,
+        Mat4,
+        [
+            [1.12, 3.04, 6.75, 9.99],
+            [8.65, 2.34, 94.6, 113.3],
+            [5.45, 6.34, 89.41, 185.1],
+            [3.56, 5.43, 77.90, 140.67]
+        ]
+    );
 }
