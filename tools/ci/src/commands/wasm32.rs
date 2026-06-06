2581be9bd8d5d1d75b2d2752c1a46671a7365c71
Cameron Hart <cameron.hart@gmail.com>
2026-06-06 22:22:38 +1200

    make the wasm32 alias only run node.

--- tools/ci/src/commands/wasm32.rs
+++ tools/ci/src/commands/wasm32.rs
@@ -4,21 +4,15 @@ use xshell::Shell;
 use crate::args::Args;
 use crate::prepare::{Prepare, PreparedCommand};
 
-use super::wasm32_chrome::Wasm32Chrome;
-use super::wasm32_firefox::Wasm32Firefox;
 use super::wasm32_node::Wasm32Node;
 
 #[derive(FromArgs, Default)]
 #[argh(subcommand, name = "wasm32")]
-/// Test with wasm-pack in all browsers (node, firefox, chrome)
+/// Test with wasm-pack in node
 pub struct Wasm32 {}
 
 impl Prepare for Wasm32 {
     fn prepare<'a>(&self, sh: &'a Shell, args: &Args) -> Vec<PreparedCommand<'a>> {
-        let mut cmds = Vec::new();
-        cmds.extend(Wasm32Node {}.prepare(sh, args));
-        cmds.extend(Wasm32Firefox {}.prepare(sh, args));
-        cmds.extend(Wasm32Chrome {}.prepare(sh, args));
-        cmds
+        Wasm32Node {}.prepare(sh, args)
     }
 }
